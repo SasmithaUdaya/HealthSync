47d1212513464303fb031813813ec92e8ff5eaad
SasmithaUdaya <sasmithaudayasri0322@gmail.com>
2025-04-26 22:17:30 +0530

    model creation and database connection

--- backend/src/main/java/backend/model/Post.java
+++ backend/src/main/java/backend/model/Post.java
@@ -1,4 +1,25 @@
 package backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "Post")
 public class Post {
+
+    @Id
+    private String postId;
+    private String postCategory;
+    private String description;
+    private String status;
+    private String duration;
+    private String postImage;
+
 }
