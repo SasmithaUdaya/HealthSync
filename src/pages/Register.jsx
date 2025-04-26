9a08219835c1c0acde77809861a94b656ab2cc3d
Thisaravi <hashinikathisaravi0523@gmail.com>
2025-04-27 01:52:14 +0530

    create register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+function Register() {
+    return (
+      <div className="text-center mt-10">
+        <h1 className="text-4xl font-bold text-purple-500">Register Page 📝</h1>
+      </div>
+    );
+  }
+  
+  export default Register;
+  
