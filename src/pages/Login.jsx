aee4754d1f5346f24e7b2bbe8927cebb456e2a6c
Thisaravi <hashinikathisaravi0523@gmail.com>
2025-04-27 01:51:46 +0530

    create login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+    return (
+      <div className="text-center mt-10">
+        <h1 className="text-4xl font-bold text-blue-500">Login Page 🔑</h1>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
