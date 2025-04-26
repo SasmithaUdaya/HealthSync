86e157753cfd12afcf7c56028abbffe7cdc77c32
Thisaravi <hashinikathisaravi0523@gmail.com>
2025-04-27 01:51:20 +0530

    create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div className="text-center mt-10">
+        <h1 className="text-4xl font-bold text-green-500">Welcome to Home Page 🏡</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
