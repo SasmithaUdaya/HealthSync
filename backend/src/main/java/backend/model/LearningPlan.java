bb356f93830bcfd4c4ce0a635f7f0d747bd99d8e
IshiniKaveesha <ishinikaveesha84@gmail.com>
2025-05-11 19:45:10 +0530

    Create post controller and post service for crud

--- backend/src/main/java/backend/model/LearningPlan.java
+++ backend/src/main/java/backend/model/LearningPlan.java
@@ -1,4 +1,37 @@
 package backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.xml.stream.events.Comment;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "LearningPlan")
 public class LearningPlan {
+
+    @Id
+    private String lpId;
+    private String lpTitle;
+    private String lpCategory;
+    private String lpDescription;
+    private String lpObjectives;
+    private String lpStatus; // e.g., Not Started, In Progress, Completed
+    private String lpDuration;
+    private String lpImage;
+    private String ownerId;
+
+    private List<String> followers;
+    private List<String> likedUserIds;
+    //private List<Comment> comments;
+    private List<String> badges;
+    private List<String> mediaFiles;
+
 }
