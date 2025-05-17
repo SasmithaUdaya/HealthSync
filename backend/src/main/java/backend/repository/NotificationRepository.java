6466c210b9257d3a7c525da67bcdf5405a528040
Ranthili Prarthana <155120972+ranthilini@users.noreply.github.com>
2025-05-17 12:09:11 +0530

    Update NotificationRepository.java

--- backend/src/main/java/backend/repository/NotificationRepository.java
+++ backend/src/main/java/backend/repository/NotificationRepository.java
@@ -2,11 +2,11 @@ package backend.repository;
 
 import backend.model.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface NotificationRepository extends MongoRepository<Notification, String> {
-    List<Notification> findByUserId(String userId);
+    List<Notification> findByPostId(String postId);
+
+    List<Notification> findByCommentId(String commentId);
 }
